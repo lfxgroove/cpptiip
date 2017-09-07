2e1f02e79a7a0a8065aee78094d918af26694f9f
Anton Sundblad <anton.sundblad@cybercom.com>
2017-09-07 11:37:23 +0200

    Actual change to c++11

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,12 +1,13 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
 #include "logging.h"
+#include "util.h"
 
 int main(int argc, char** argv) {
         doctest::Context ctx;
 
         // Turn off logging
-        logging::Log::root().setDest(std::make_unique<logging::DummyDest>());
+        logging::Log::root().setDest(util::make_unique<logging::DummyDest>());
         
         ctx.applyCommandLine(argc, argv);
         int res = ctx.run();
